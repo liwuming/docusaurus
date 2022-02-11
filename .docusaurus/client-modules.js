6b070b3741ef9235db62a7316011f3b6c95cea99
liwuming <1459543371@qq.com>
2022-02-11 11:58:23 +0800

    asa

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('E:\\myblog\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('E:\\myblog\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('E:\\myblog\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
+  require('E:\\myblog\\src\\css\\custom.css'),
+];
