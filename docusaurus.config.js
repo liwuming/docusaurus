bc8d6bbc3a33e3d83ca73ec14b6bbfc2e48a22b5
liwuming <1459543371@qq.com>
2022-03-10 23:20:36 +0800

    asa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "liwuming's blog",
   tagline: 'Dinosaurs are cool',
   url: 'https://mydocs.ibiancheng.net',
-  baseUrl: '/docusaurus/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
